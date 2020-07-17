b8441043baba9de957ce37065d57876402dd6c0c
Watson-Sei <seinabehack@gmail.com>
2020-07-18 02:02:35 +0900

    environ secret key

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -16,11 +16,15 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
+import environ
+env = environ.Env()
+env.read_env(os.path.join(BASE_DIR, '.env'))
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '&6o5m6jra&9xa(=xf#%zt=!+#5@^n_n-l&f0u@ipcb+eo$m1*e'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
